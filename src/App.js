e42f7966da945d32faf918a93a8d1a034b57cf20
vadickpb <vadick1@gmail.com>
2020-12-19 21:44:52 -0500

    agregando route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+
+
+const Hats = () => {
+  return (
+
+    <div>
+        <h1>HATS</h1>
+    </div>
+
+  )
+}
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path = '/' component = {HomePage} />
+      <Route path = '/hats' component = {Hats} />
     </div>
   );
 }
